5e8e6b4a501af07e70b202ceb749a652764cc812
radearsr <radea.surya1@gmail.com>
2023-08-13 05:54:54 +0700

    redux implement memoization

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,17 @@
-export const selectCategoriesMap = (state) => state.categories.categories
-.reduce((acc, docSnapshot) => {
-  const { title, items } = docSnapshot;
-  acc[title.toLowerCase()] = items;
-  return acc;
-}, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categorySlice) => categorySlice.categories,
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {}),
+);
